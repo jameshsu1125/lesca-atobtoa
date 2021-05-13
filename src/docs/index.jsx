a72483eb4c3619bf8f357b2ee406c9bc733346d0
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-05-14 00:48:15 +0800

    add push value

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
 import MyClass from './../lib/index';
-const Data = [{ name: 'james', age: 18 }];
-const base64 = MyClass.toBase64(Data);
-console.log(base64);
-console.log(MyClass.toJson(base64));
 
-import { toBase64, toJson } from './../lib/index';
-console.log(toBase64, toJson);
+const pushData = 3;
+const Data = [{ name: 'james', age: 18 }];
+const base64 = MyClass.toBase64(Data, pushData);
+const result = MyClass.toJson(base64, pushData);
+console.log(base64);
+console.log(result);
 
 function Demo() {
 	return <></>;
--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,10 +1,32 @@
 const atobtoa = {
-	toBase64(data) {
-		return btoa(encodeURIComponent(escape(JSON.stringify(data))));
+	/**
+	 * toBase64({ name:'james', age:18 }, 3);
+	 * @param {*} data
+	 * @param {number} len
+	 * @returns {string} base64 => MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@
+	 */
+	toBase64(data, push = 0) {
+		let result = '';
+		btoa(encodeURIComponent(escape(JSON.stringify(data))))
+			.split('')
+			.forEach((e) => {
+				result += String.fromCharCode(e.charCodeAt() + push);
+			});
+		return result;
 	},
-	toJson(str) {
+	/**
+	 * toJson('MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@', 3);
+	 * @param {string} str
+	 * @param {number} len
+	 * @returns // => { name:'james', age:18 }
+	 */
+	toJson(str, push = 0) {
 		try {
-			return JSON.parse(unescape(decodeURIComponent(window.atob(str))));
+			let result = '';
+			str.split('').forEach((e) => {
+				result += String.fromCharCode(e.charCodeAt() - push);
+			});
+			return JSON.parse(unescape(decodeURIComponent(window.atob(result))));
 		} catch {
 			return 'format error';
 		}
