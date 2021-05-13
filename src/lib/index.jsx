f63a1bdb168ed9900a293d40d47afd705a1c7cea
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-05-14 01:43:54 +0800

    to es6

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,36 +1,35 @@
-const atobtoa = {
-	/**
-	 * toBase64({ name:'james', age:18 }, 3);
-	 * @param {*} data
-	 * @param {number} len
-	 * @returns {string} base64 => MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@
-	 */
-	toBase64(data, push = 0) {
-		let result = '';
-		btoa(encodeURIComponent(escape(JSON.stringify(data))))
-			.split('')
-			.forEach((e) => {
-				result += String.fromCharCode(e.charCodeAt() + push);
-			});
-		return result;
-	},
-	/**
-	 * toJson('MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@', 3);
-	 * @param {string} str
-	 * @param {number} len
-	 * @returns // => { name:'james', age:18 }
-	 */
-	toJson(str, push = 0) {
-		try {
-			let result = '';
-			str.split('').forEach((e) => {
-				result += String.fromCharCode(e.charCodeAt() - push);
-			});
-			return JSON.parse(unescape(decodeURIComponent(window.atob(result))));
-		} catch {
-			return 'format error';
-		}
-	},
+/**
+ * toBase64({ name:'james', age:18 }, 3);
+ * @param {*} data
+ * @param {number} len
+ * @returns {string} base64 => MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@
+ */
+const toBase64 = (data, push = 0) => {
+	let result = '';
+	btoa(encodeURIComponent(escape(JSON.stringify(data))))
+		.split('')
+		.forEach((e) => {
+			result += String.fromCharCode(e.charCodeAt() + push);
+		});
+	return result;
 };
-export default atobtoa;
-export const { toBase64, toJson } = atobtoa;
+
+/**
+ * toJson('MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@', 3);
+ * @param {string} str
+ * @param {number} len
+ * @returns // => { name:'james', age:18 }
+ */
+const toJson = (str, push = 0) => {
+	try {
+		let result = '';
+		str.split('').forEach((e) => {
+			result += String.fromCharCode(e.charCodeAt() - push);
+		});
+		return JSON.parse(unescape(decodeURIComponent(window.atob(result))));
+	} catch {
+		return 'format error';
+	}
+};
+
+export default { toBase64, toJson };
