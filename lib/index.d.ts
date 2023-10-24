b9eb4cbb1ac2f30958ba9290000d99e9ecd27cc8
jameshsu1125 <jameshsu1125@gmail.com>
2023-10-24 14:17:48 +0800

    add babel runtime and upgrade

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,19 +2,19 @@
  * toBase64({ name:'james', age:18 }, 3);
  * @param {object} data
  * @param {number} len
- * @returns {string} base64
+ * @returns {string} base64 => MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@
  */
 declare const toBase64: (data: object, push?: number) => string;
 /**
- * toJson('base64', 3);
+ * toJson('MWL4QXLoPmX6TlX|QWL|epIw]VX|QWL|MWL4P3HoPmX|PpskeZY}MWL4PmLoPmX|T|X|QWL|\ZgoMWL4PmLoPmX}TWH7MWL4Q3ToPmX4UD@@', 3);
  * @param {string} str
  * @param {number} len
  * @returns // => { name:'james', age:18 }
  */
 declare const toJson: (str: string, push?: number) => any;
 declare const Atobtoa: {
-  toBase64: (data: object, push?: number) => string;
-  toJson: (str: string, push?: number) => any;
+    toBase64: (data: object, push?: number) => string;
+    toJson: (str: string, push?: number) => any;
 };
 export default Atobtoa;
 export { toBase64, toJson };
